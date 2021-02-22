a5d7109f8ad032d6aa91c9c2f173ba514b4dd76b
Calvin Conley <cconley0125@gmail.com>
2021-02-22 18:51:53 -0500

    added CRU functionality for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :posts_categories, class_name: "PostCategory"
   has_many :posts, through: :posts_categories
+
+  validates :name, presence: true
 end
