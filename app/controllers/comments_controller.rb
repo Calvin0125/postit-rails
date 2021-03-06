f0c05e56067b96c545d83bf063ab7e020c0dcd66
Calvin Conley <cconley0125@gmail.com>
2021-03-06 16:30:03 -0500

    added capability to vote on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,31 @@
 class CommentsController < ApplicationController
   before_action :require_user
-  
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.user_id = 1
+    @comment.user_id = current_user.id
 
     if @comment.save
       flash[:notice] = "Comment was successfully created"
       redirect_to post_path(@post)
     else
+      # Deletion is necessary, otherwise the comments partial tries to
+      # render the in-memory comment and throws an error
+      @post.comments.delete(@comment)
       render 'posts/show'
+    end
+  end
+
+  def vote
+    @comment = Comment.find(params[:id])
+    vote = Vote.new(vote: params[:vote], user_id: current_user.id, voteable: @comment)
+    if vote.save
+      flash[:notice] = "Your vote on the comment has been counted."
+      redirect_to :back
+    else
+      flash[:error] = "Your vote on the comment was not counted."
+      redirect_to :back
     end
   end
 
